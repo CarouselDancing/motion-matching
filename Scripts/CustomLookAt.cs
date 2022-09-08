9fc1621c798cf599438bcfe4d7e04763f8cf6720
Erik Herrmann <erik.herrmann@dfki.de>
2022-09-08 19:01:54 +0200

    Added support for rotation limits to custom look at

--- Scripts/CustomLookAt.cs
+++ Scripts/CustomLookAt.cs
@@ -11,8 +11,12 @@ public class CustomLookAt : CharacterPoser
     public Transform boneTransform;
     public HumanBodyBones targetBone = HumanBodyBones.Head;
     public Transform targetTransform;
-    public Vector3 positionnOffset = new Vector3(0,-0.3f,0);
+    public Vector3 positionOffset = new Vector3(0,-0.3f,0);
     public Vector3 rotationOffset;
+    public Vector3 localDesiredRotationE;
+    public Quaternion defaultRotation;
+    public Vector3 maxAngles = new Vector3(90,90,90);
+    public bool applyLimits= true;
 
     void Start()
     {
@@ -24,16 +28,37 @@ public class CustomLookAt : CharacterPoser
     {
         boneTransform = anim.GetBoneTransform(bone);
         initialized = boneTransform != null;
+        if(initialized) defaultRotation = boneTransform.localRotation;
+    }
+
+    public float ClampAngle(float angle, float maxAngle){
+            if(Mathf.Abs(angle) > 180) {
+                if(angle < 0){
+                    angle = (360+angle) % 360;
+                }else{
+                    angle = -(360-angle) % 360;
+                }
+            }
+            angle = Mathf.Clamp(angle, -maxAngle, maxAngle);
+            return angle;
     }
 
 
      override public void UpdatePose()
     {   
         if (targetTransform == null) return;
-        var direction = (targetTransform.position) - boneTransform.position+positionnOffset;
+        var direction = (targetTransform.position) - boneTransform.position+positionOffset;
         Quaternion desiredRotation = Quaternion.LookRotation(direction.normalized, Vector3.up);
+        if (applyLimits){
+            var localDesiredRotation  = Quaternion.Inverse(boneTransform.parent.rotation)*Quaternion.Inverse(defaultRotation)*  desiredRotation;
+            localDesiredRotationE = localDesiredRotation.eulerAngles;
+            localDesiredRotationE.x = ClampAngle(localDesiredRotationE.x, maxAngles.x);
+            localDesiredRotationE.y = ClampAngle(localDesiredRotationE.y, maxAngles.y);
+            localDesiredRotationE.z = ClampAngle(localDesiredRotationE.z, maxAngles.z);
+            localDesiredRotation = Quaternion.Euler(localDesiredRotationE.x, localDesiredRotationE.y, localDesiredRotationE.z);
+            desiredRotation = boneTransform.parent.rotation * defaultRotation * localDesiredRotation;
+        }
         var offsetQ = Quaternion.Euler(rotationOffset.x, rotationOffset.y, rotationOffset.z);
-        //var rotation = Quaternion.FromToRotation(m.dstUp, globalSrcUp).normalized;
         boneTransform.rotation = desiredRotation*offsetQ;
 
         
@@ -43,7 +68,6 @@ public class CustomLookAt : CharacterPoser
     void FixedUpdate()
     {
         if(!active || inPipeline)return;
-        //transform.position = src.GetGlobalPosition(Bones.Entity);
         if (!initialized) SetTransforms();
         UpdatePose();
     }
